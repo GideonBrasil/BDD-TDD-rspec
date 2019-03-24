6f3130404bc3bd7fc59a071bc54e3e91af6f5807
GideonBrasil <gidxx_z@yahoo.com>
2019-03-24 15:36:31 +0000

    validation tests ready to run and auction class

--- rails_models_test/config/environments/development.rb
+++ rails_models_test/config/environments/development.rb
@@ -12,6 +12,10 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.generators do |g|
+    g.test_framework :rspec
+  end
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
