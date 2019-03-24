0cc971969dd319d318da66dc4b9e08a29a2bcf1c
GideonBrasil <gidxx_z@yahoo.com>
2019-03-24 13:41:41 +0000

    first tests using describe method

---
+++ ketchup.rb
@@ -0,0 +1,40 @@
+# describe Burger do
+#   describe "#apply_ketchup" do
+#     context "with ketchup" do
+#       let(:burger) { Burger.new(:ketchup => true) }
+#       before  { burger.apply_ketchup }
+
+#       it "sets the ketchup flag to true" do
+#         burger.has_ketchup_on_it?.should be_true
+#       end
+#     end
+
+#     context "without ketchup" do
+#       let(:burger) { Burger.new(:ketchup => false) }
+#       before  { burger.apply_ketchup }
+
+#       it "sets the ketchup flag to false" do
+#         burger.has_ketchup_on_it?.should be_false
+#       end
+#     end
+#   end
+# end
+
+describe Burger do
+  describe "#apply_ketchup" do
+    subject { burger }
+    before  { burger.apply_ketchup }
+
+    context "with ketchup" do
+      let(:burger) { Burger.new(:ketchup => true) }
+
+      specify { subject.has_ketchup_on_it?.should be_true }
+    end
+
+    context "without ketchup" do
+      let(:burger) { Burger.new(:ketchup => true) }
+
+      specify { subject.has_ketchup_on_it?.should be_false }
+    end
+  end
+end
