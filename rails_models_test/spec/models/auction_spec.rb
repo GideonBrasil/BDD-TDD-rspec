5d673bda3350840f89d405ec2ca0d8f27f9d83d5
GideonBrasil <gidxx_z@yahoo.com>
2019-03-24 15:39:39 +0000

    test for valid attributes

--- rails_models_test/spec/models/auction_spec.rb
+++ rails_models_test/spec/models/auction_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Auction, type: :model do
-  it "is valid with valid attributes"
-  it "is not valid without a title"
   it "is not valid without a description"
   it "is not valid without a start_date"
   it "is not valid without a end_date"
@@ -11,4 +9,9 @@ RSpec.describe Auction, type: :model do
     auction = Auction.new(title: nil)
     expect(auction).to_not be_valid
   end
+
+  it "is valid with valid attributes" do
+    expect(Auction.new(title: 'Anything')).to be_valid
+  end
+
 end
