706362d4ec25c35dd01fa5fabf3a362b1d074211
GideonBrasil <gidxx_z@yahoo.com>
2019-03-24 16:02:36 +0000

    add appropriate associations to auction model

--- rails_models_test/app/models/auction.rb
+++ rails_models_test/app/models/auction.rb
@@ -1,5 +1,9 @@
 class Auction < ApplicationRecord
 
-    validates_presence_of :title
-    
+    has_one :buyer, class_name: "User"
+    has_one :seller, class_name: "User"
+
+    validates_presence_of :title, :description, :start_date, :end_date
+
+
 end
